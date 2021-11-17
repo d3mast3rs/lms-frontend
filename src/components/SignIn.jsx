209f5dcacedb519434f9a5f1a386960401cd0647
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2021-11-17 14:10:57 +0530

    project setup initial

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/SignIn.css";
+
+const SignIn = () => {
+  return <div className="signin__main">oeheo herohnioehnoen login</div>;
+};
+
+export default SignIn;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/SignUp.css';
+
+const SignUp = () => {
+    return (
+        <div className="signup__main">
+            This is the signup page.
+        </div>
+    )
+}
+
+export default SignUp
